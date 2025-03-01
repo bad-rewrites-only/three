590795c3e15737e1fb54636b8dc7b0cd3bed8492
Tao Tien <29749622+taotien@users.noreply.github.com>
2025-03-01 11:03:34 -0800

    begin transition to iced

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,12 @@
-use serde::{Deserialize, Serialize};
+use crate::Three;
 
-#[derive(Default, Serialize, Deserialize)]
-pub struct ThreeApp {}
-
-impl ThreeApp {
-    pub fn name() -> &'static str {
-        "three"
-    }
+#[derive(Debug, Clone)]
+enum Message {
+    Post,
 }
 
-impl eframe::App for ThreeApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        ctx.set_pixels_per_point(1.5);
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("This is a ui.heading. ");
-
-            ui.label("This is a ui.label");
-
-            // This literally creates the button AND checks to see if it was clicked
-            if ui.button("Quit").clicked() {
-                std::process::exit(0);
-            };
-        });
+impl Three {
+    pub fn title(&self) -> String {
+        "three".into()
     }
 }
